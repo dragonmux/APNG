0ed1e6e3c4612d0077f754ded26b774ffb7caec1
dx-mon <dx-mon@users.sourceforge.net>
2019-10-18 22:21:19 -0400

    Fixed a little cruft in the CRC32 header

--- crc32.hxx
+++ crc32.hxx
@@ -3,7 +3,7 @@
 
 #include "internals.hxx"
 
-constexpr uint32_t calcPolynomial(const uint8_t bit) noexcept { return 1U << (31 - bit); }
+constexpr uint32_t calcPolynomial(const uint8_t bit) noexcept { return 1U << (31U - bit); }
 template<typename T, typename... U> constexpr typename std::enable_if<sizeof...(U) != 0, uint32_t>::type calcPolynomial(T bit, U... bits) noexcept
 	{ return calcPolynomial(bit) | calcPolynomial(bits...); }
 
@@ -13,8 +13,6 @@ private:
 	constexpr static const uint32_t poly = calcPolynomial(0, 1, 2, 4, 5, 7, 8, 10, 11, 12, 16, 22, 23, 26);
 	static_assert(poly == 0xEDB88320u, "Polynomial calculation failure");
 	static std::array<const uint32_t, 256> crcTable;
-
-	crc32_t() = delete;
 
 public:
 	template<size_t N> static void crc(uint32_t &crc, const std::array<uint8_t, N> &data) noexcept
@@ -26,6 +24,8 @@ public:
 			crc = crcTable[uint8_t(crc ^ *data++)] ^ (crc >> 8U);
 		crc ^= UINT32_C(0xFFFFFFFF);
 	}
+
+	crc32_t() = delete;
 };
 
 #endif /*CRC32_HXX*/
