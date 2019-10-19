17b2849bdaa8fabc2108897fc4af6d431fd9a30f
dx-mon <dx-mon@users.sourceforge.net>
2019-10-19 09:39:55 -0400

    Updated the debian build script to use the current stable compiler

--- buildDebian64.sh
+++ buildDebian64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-USE_ARCH=64 PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig" GCC="gcc-5 -m64 -fPIC -DPIC" GXX="g++-5 -m64 -fPIC -DPIC" LIBDIR=/usr/lib/x86_64-linux-gnu make "$@"
+USE_ARCH=64 PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig" GCC="gcc-8 -m64 -fPIC -DPIC" GXX="g++-8 -m64 -fPIC -DPIC" LIBDIR=/usr/lib/x86_64-linux-gnu make "$@"
